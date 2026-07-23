f706b6fbe734f09cf134a459ccacb89488d1519b
Dhruv garg <122282395+dhruvg-7@users.noreply.github.com>
2026-07-23 19:06:26 +0530

    Remove alert for script working confirmation
    Removed alert message indicating script functionality.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const items=[['Announcements','icons/announcement.svg'],['Approvals','icons/approval.svg'],['Documents','icons/document.svg'],['Asset Management','icons/asset.svg'],['FAQs','icons/faq.svg']];
 const cards=document.getElementById('cards');
-alert("Script is working!");
 items.forEach(async i=>{const t=await fetch(i[1]).then(r=>r.text());const d=document.createElement('div');d.className='card';d.innerHTML='<div class="icon">'+t+'</div><div><b>'+i[0]+'</b></div>';cards.appendChild(d);});
 function apply(c){document.documentElement.style.setProperty('--primary',c);document.querySelectorAll('.icon').forEach(x=>x.style.color=c);}
 picker.oninput=()=>{hex.value=picker.value;apply(picker.value)};hex.oninput=()=>{apply(hex.value);if(/^#[0-9A-Fa-f]{6}$/.test(hex.value))picker.value=hex.value};apply('#1976D2');
